288dcbe859fd5b809813117ee2564e76675ef58e
keryan <keryanbelahcene@gmail.com>
2025-04-30 09:41:10 +0200

    Fixed Display Protocol

--- hardware/display.py
+++ hardware/display.py
@@ -1,10 +1,10 @@
 from luma.core.interface.serial import i2c
-from luma.oled.device import ssd1306
+from luma.oled.device import sh1106
 from luma.core.render import canvas
 from PIL import ImageFont
 
 serial = i2c(port=1, address=0x3C)
-device = ssd1306(serial)
+device = sh1106(serial)
 
 def display_message(message, duration=3):
     with canvas(device) as draw:
